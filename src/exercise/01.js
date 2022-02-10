1887df04e35737401fe557798a20f7fcb78b362a
Ronald Arias <ariasron@hotmail.com>
2022-02-10 09:00:06 -0500

    Simulates setState with an object or function

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,13 +3,16 @@
 
 import * as React from 'react'
 
-const countReducer = (state, action) => ({...state, ...action})
+const countReducer = (state, action) => ({
+  ...state,
+  ...(typeof action === 'function' ? action(state) : action)
+})
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = React.useReducer(countReducer, {count: initialCount})
   const {count} = state
-  
-  const increment = () => setState({count: count + step})
+
+  const increment = () => setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
