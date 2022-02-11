c93fcdf94b388e4d602dc911277170e2157eee06
Ronald Arias <ariasron@hotmail.com>
2022-02-11 10:51:20 -0500

    Implements traditional dispatch object with a type and switch statement

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,16 +3,26 @@
 
 import * as React from 'react'
 
-const countReducer = (state, action) => ({
-  ...state,
-  ...(typeof action === 'function' ? action(state) : action)
-})
+const countReducer = (state, action) => {
+  console.log(state)
+  const { type, step } = action
+  switch(type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        count: state.count + step
+      }
+    default: {
+        throw new Error(`Unsupported action type: ${type}`)
+      }
+  }
+}
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {count: initialCount})
+  const [state, dispatch] = React.useReducer(countReducer, {count: initialCount})
   const {count} = state
 
-  const increment = () => setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
