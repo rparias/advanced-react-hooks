f0789d82a22a4df9b63c9ca62ca7d549ee100938
Ronald Arias <ariasron@hotmail.com>
2022-02-21 20:14:33 -0500

    useDebugValue: uses the format function

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,9 +3,11 @@
 
 import * as React from 'react'
 
+const formatDebugValue = ({query, state}) => `\`${query}\` => ${state}`
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
-  React.useDebugValue(`\`${query}\` => ${state}`)
+  React.useDebugValue({query, state}, formatDebugValue)
 
   React.useEffect(() => {
     let mounted = true
